871f5a2034b6300253ba517726e6115afee965e6
Ramin Mammadzada <remki-design@Hotmail.com>
2021-01-19 00:17:38 +0300

    Build dist folder and files for production.

--- src/javascript/helpers/uppercaseFirstLetter.js
+++
@@ -1,7 +0,0 @@
-const uppercaseFirstLetter = (string) => {
-  const firstLetter = string.charAt(0).toUpperCase();
-  return firstLetter + string.slice(1).toLowerCase();
-};
-
-
-export default uppercaseFirstLetter;
--- src/javascript/view/generateFooter.js
+++
@@ -1,8 +0,0 @@
-const footerView = () => {
-  const footer = document.createElement('footer');
-  footer.innerHTML = '<p>designed with ♥️ by Ramin Mammadzada</p>';
-  return footer;
-};
-
-
-export default footerView;
