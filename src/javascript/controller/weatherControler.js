0c1b25af11ed1b8a4ca7739c60b7eb59b5634f9e
Ramin Mammadzada <remki-design@Hotmail.com>
2021-01-18 19:11:32 +0300

    Change the order of async function calls to populate all info almost at once.

--- src/javascript/controller/weatherControler.js
+++ src/javascript/controller/weatherControler.js
@@ -3,6 +3,10 @@ import changeTextColors from '../helpers/changeTextColors';
 
 const { fetchWeatherData } = require('../model/weatherData');
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const unsplash = createApi({
   accessKey: 'RmRQetkGdLG3o6Dv0LTcQ9-7Fvi6X1FoTF6Ax_pDsV4',
   fetch: global.fetch,
@@ -26,7 +30,7 @@ async function fetchImageData(queryText) {
   return [img, avarageColorCode];
 }
 
-async function updateBody(queryText) {
+async function updateBodyBackground(queryText) {
   const [returnedImage, avarageColorCode] = await fetchImageData(queryText);
 
   const body = document.querySelector('body');
@@ -35,7 +39,7 @@ async function updateBody(queryText) {
 
   changeTextColors(avarageColorCode);
 
-  // console.log(">3> thirdly run this");
+  console.log(">1> firstly run this");
 }
 
 const populateMain = () => {
@@ -51,6 +55,7 @@ const populateMain = () => {
   let weatherCondition;
 
   const setTemperatureView = () => {
+    console.log(">2> secondly run this");
     console.log(tempInF);
     if (tempInF === undefined) {
       temperateView.innerText = "You must first check weather for city!";
@@ -72,11 +77,11 @@ const populateMain = () => {
     // [, temperateView.innerText] = Math.round(temperatures);
     [tempInC, tempInF, weatherCondition] = weatherData;
 
-    setTemperatureView();
-
     weatherDescription.innerText = weatherCondition.description;
     weatherIcon.src = 'http://openweathermap.org/img/w/' + weatherCondition.icon.toString() + '.png';
-    updateBody(weatherCondition.main);
+    await updateBodyBackground(weatherCondition.main);
+    await sleep(1500);
+    await setTemperatureView();
     // console.log(">1> first run this");
   };
 
